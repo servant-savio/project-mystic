e6de2b4fb5a60c22fb89b19cef70c2d8c9b8b6e9
Eric de Guzman <eric_deguzman@bmc.com>
2024-05-26 06:42:13 -0700

    glorious

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# project-mystic
-test
+# Project Mystic
+
+![alt text](./images/mystic-logo.svg)
+
+Follow us
+
+* <a href="https://x.com/ServantSavio"><img src="./images/ServantSavioX.png" /></a>
+* <a href="https://x.com/ProjectMysticDV"><img src="./images/ProjectMysticDVX.png" /></a>
--- rosary/scripts.js
+++ rosary/scripts.js
@@ -1,28 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let touchstartX = 0;
-  let touchendX = 0;
+  const touchstart = {X:0, Y:0};
+  const touchend = {X:0, Y:0};
+
+  function isXorY (startX, endX, startY, endY) {
+    const deltaX = Math.abs(startX - endX);
+    const deltaY = Math.abs(startY - endY);
+    return (deltaX > deltaY);
+  }
 
   function handleGesture() {
-      if (touchendX < touchstartX) {
-          const nextLink = document.getElementById('next');
-          if (nextLink) {
-              window.location.href = nextLink.href;
-          }
-      }
-      if (touchendX > touchstartX) {
-          const prevLink = document.getElementById('prev');
-          if (prevLink) {
-              window.location.href = prevLink.href;
-          }
-      }
+    let nextId, prevId;
+    const XorY = isXorY(touchstart.X, touchend.X, touchstart.Y, touchend.Y);
+    nextId = `next${XorY}`;
+    prevId = `prev${XorY}`;
+    const currentTouchstart = touchstart[XorY]; 
+    const currentTouchend = touchend[XorY]; 
+
+    if (currentTouchend < currentTouchstart) {
+        const nextLink = document.getElementById(nextId);
+        if (nextLink) {
+            window.location.href = nextLink.href;
+        }
+    } else if (currentTouchend > currentTouchstart) {
+        const prevLink = document.getElementById(prevId);
+        if (prevLink) {
+            window.location.href = prevLink.href;
+        }
+    }
   }
 
   document.addEventListener('touchstart', function(event) {
-      touchstartX = event.changedTouches[0].screenX;
+    touchstart.X = event.changedTouches[0].screenX;
+    touchstart.Y = event.changedTouches[0].screenY;
   }, false);
 
   document.addEventListener('touchend', function(event) {
-      touchendX = event.changedTouches[0].screenX;
-      handleGesture();
+    touchend.X = event.changedTouches[0].screenX;
+    touchend.Y = event.changedTouches[0].screenY;
+    handleGesture();
   }, false);
 });
